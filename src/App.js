2b2f7c648e905af70e32492c858d3eec2f5bbfa4
HorusCh <37905833+horus0523@users.noreply.github.com>
2023-11-27 17:51:45 -0300

    Diseño de la interfaz gráfica

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleImageAnalysis = () => {
+    // Aquí irá el código para analizar la imagen
+  };
+
+  const handleImageGeneration = () => {
+    // Aquí irá el código para generar la imagen
+  };
+
+  return (
+    <div className="app">
+      <h1 className="title">Computer vision</h1>
+      <input
+        type="text"
+        className="input"
+        placeholder="Enter URL to analyze or textual prompt to generate an image"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <div className="buttons">
+        <button className="button" onClick={handleImageAnalysis}>
+          Analyze
+        </button>
+        <button className="button" onClick={handleImageGeneration}>
+          Generate
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
